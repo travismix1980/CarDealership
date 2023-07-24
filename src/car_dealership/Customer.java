ed1d43ca05cb861650e7a48a9cf14673cc1cedf5
Travis Mix <travismix1980@gmail.com>
2023-07-23 21:21:24 -0700

    Initial Commit

---
+++ src/car_dealership/Customer.java
@@ -0,0 +1,10 @@
+package car_dealership;
+
+public class Customer {
+    String name;
+    String address;
+    double cashOnHand;
+    public void purchaseCar(Vehicle vehicle, Employee employee, boolean finance){
+
+    }
+}
---
+++ src/car_dealership/Dealership.java
@@ -0,0 +1,9 @@
+package car_dealership;
+
+// Write a program to simulate a car dealership sales process
+// We will have employees, vehicles, and customers
+public class Dealership {
+    public static void main(String[] args) {
+        Customer cust1 = new Customer();
+    }
+}
